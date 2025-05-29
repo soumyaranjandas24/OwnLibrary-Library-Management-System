4bb9e5c8bc213e35092c3fbaacabee314c210b1c
soumyaranjandas24 <soumyaranjan24@outlook.com>
2025-05-29 23:59:39 +0530

    Add files via upload

---
+++ book_catagory/edit_cat.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+    #fetch data from database
+    $connection = mysqli_connect("localhost","root","");
+    $db = mysqli_select_db($connection,"OwnLibrary");
+    $cat_id = "";
+    $cat_name = "";
+    $query = "select * from category where cat_id = $_GET[cid]";
+    $query_run = mysqli_query($connection,$query);
+    while ($row = mysqli_fetch_assoc($query_run)){
+        $cat_name = $row['cat_name'];
+        $cat_id = $row['cat_id'];
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="../admin/admin_dashboard.php">OwnLibrary</a>
+            </div>
+            <font style="color: white"><span><strong>Welcome: <?php echo $_SESSION['name'];?></strong></span></font>
+            <font style="color: white"><span><strong>Email: <?php echo $_SESSION['email'];?></strong></font>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown">My Profile </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="">View Profile</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Edit Profile</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="../admin/change_password.php">Change Password</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../logout.php">Logout</a>
+              </li>
+            </ul>
+        </div>
+    </nav><br>
+    <span><marquee>This is library mangement system. Library opens at 8:00 AM and close at 8:00 PM</marquee></span><br><br>
+        <center><h4>Edit Book</h4><br></center>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="name">Category Name:</label>
+                        <input type="text" class="form-control" name="cat_name" value="<?php echo $cat_name; ?>" required>
+                    </div>
+                    <button type="submit" name="update_cat" class="btn btn-primary my-2">Update Catogry</button>
+                </form>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php
+    if(isset($_POST['update_cat'])){
+        $connection = mysqli_connect("localhost","root","");
+        $db = mysqli_select_db($connection,"OwnLibrary");
+        $query = "update category set cat_name = '$_POST[cat_name]' where cat_id = $_GET[cid]";
+        $query_run = mysqli_query($connection,$query);
+        header("location:manage_cat.php");
+    }
+?>
